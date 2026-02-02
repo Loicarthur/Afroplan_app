3e701ca6c9d3c9b0cd8cbba370dff76d16656c75
Loicarthur <tchuenkamarthur26@yahoo.com>
2026-02-02 15:14:34 +0100

    feat: update dependencies and remove unused files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import 'react-native-url-polyfill/auto';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
 import 'react-native-reanimated';
+
+
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { AuthProvider } from '@/contexts/AuthContext';
